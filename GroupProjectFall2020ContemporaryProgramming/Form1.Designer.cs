7c89036295b65a875c61957a69c8b13f1b8d1d52
nicomp42 <nicholdw@ucmail.uc.edu>
2020-10-22 11:15:22 -0400

    Adding controls to form

--- GroupProjectFall2020ContemporaryProgramming/Form1.Designer.cs
+++ GroupProjectFall2020ContemporaryProgramming/Form1.Designer.cs
@@ -28,13 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.lblFirstName = new System.Windows.Forms.Label();
+            this.lbleMailAddress = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.Location = new System.Drawing.Point(177, 35);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(58, 13);
+            this.lblLastName.TabIndex = 0;
+            this.lblLastName.Text = "Last Name";
+            this.lblLastName.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblFirstName
+            // 
+            this.lblFirstName.AutoSize = true;
+            this.lblFirstName.Location = new System.Drawing.Point(177, 58);
+            this.lblFirstName.Name = "lblFirstName";
+            this.lblFirstName.Size = new System.Drawing.Size(57, 13);
+            this.lblFirstName.TabIndex = 1;
+            this.lblFirstName.Text = "First Name";
+            // 
+            // lbleMailAddress
+            // 
+            this.lbleMailAddress.AutoSize = true;
+            this.lbleMailAddress.Location = new System.Drawing.Point(180, 92);
+            this.lbleMailAddress.Name = "lbleMailAddress";
+            this.lbleMailAddress.Size = new System.Drawing.Size(73, 13);
+            this.lbleMailAddress.TabIndex = 2;
+            this.lbleMailAddress.Text = "eMail Address";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbleMailAddress);
+            this.Controls.Add(this.lblFirstName);
+            this.Controls.Add(this.lblLastName);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.Label lblFirstName;
+        private System.Windows.Forms.Label lbleMailAddress;
     }
 }
 
--- GroupProjectFall2020ContemporaryProgramming/Form1.cs
+++ GroupProjectFall2020ContemporaryProgramming/Form1.cs
@@ -16,5 +16,10 @@ namespace GroupProjectFall2020ContemporaryProgramming
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
